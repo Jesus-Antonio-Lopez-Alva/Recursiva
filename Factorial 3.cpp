c514ca2e6e4433a5477b5fbe83d9baa8d24f5dc3
Jesús Antonio López Alva <86020187+Jesus-Antonio-Lopez-Alva@users.noreply.github.com>
2021-06-21 20:49:01 -0500

    Add files via upload
    Programa en lenguaje C de un factorial de una funcion recursiva

---
+++ Factorial 3.cpp
@@ -0,0 +1,27 @@
+#include 
+using std::cout;
+using std::cin;
+using std::endl;
+long int factorial (int); //prototipo de funcion
+int main()
+{
+  int x = 0; //Declaracio e inicializacion en 0.
+  cout << "*Ingrese x para calcular x! (factorial de x)*" << endl;
+  cin >> x;
+  cout << "El resultado es, " << x <<"! = "<< factorial(x) << endl; //Llamada a la funcion
+}
+long int factorial (int y ) //funcion mayor
+{
+  switch(y)
+    {
+    case 0:
+      return 1;
+      break;
+    case 1:
+      return 1;
+      break;
+    default:
+      return (factorial(y-1)*y); //Llamada recursiva, la funcion se llama asi misma
+      break;
+    }
+}
